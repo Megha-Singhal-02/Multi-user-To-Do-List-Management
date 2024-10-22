0596c1cec59f63d980ec1c2fb9c6014aa17e0f57
Megha-Singhal-02 <singhalmegha784@gmail.com>
2024-10-22 23:49:41 +0530

    added

--- backend/database/database.js
+++ backend/database/database.js
@@ -3,11 +3,15 @@ const session = require('express-session');
 const MySQLStore = require('express-mysql-session')(session);
 
 const connection = mysql.createConnection({
-	host: process.env.DB_HOST,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_NAME,
-	port: 3306,
+	// host: process.env.DB_HOST,
+	// user: process.env.DB_USER,
+	// password: process.env.DB_PASSWORD,
+	// database: process.env.DB_NAME,
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'todolist',
+	port: 3307,
 	connectTimeout: 20000
 });
 
